dd289c1312ac79455f92a5a5155ce5d443177515
Illia-pro2025 <illamyrovec05@gmail.com>
2026-09-02 22:22:32 +0200

    Updates

---
+++ projec priver.0bbb8146.js
@@ -0,0 +1,2 @@
+fetch("https://api.anilibria.app/v3/title/releases").then(e=>e.json()).then(e=>{console.log("Base d'anime reçue :",e)}).catch(e=>{console.error("Erreur lors de la récupération :",e)});
+//# sourceMappingURL=projec priver.0bbb8146.js.map
