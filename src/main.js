ca8afbf13027abdb5993e7c65542c9993fefd3d6
Illia-pro2025 <illamyrovec05@gmail.com>
2026-09-03 19:44:40 +0200

    commit

--- src/main.js
+++ src/main.js
@@ -2,9 +2,5 @@ const API_URL = "https://my-projet-production.up.railway.app/releases";
 
 fetch(API_URL)
   .then((res) => res.json())
-  .then((data) => {
-    console.log("Base d'anime reçue :", data);
-  })
-  .catch((err) => {
-    console.error("Erreur backend :", err);
-  });
+  .then((data) => console.log(data))
+  .catch((err) => console.error("Erreur backend :", err));
